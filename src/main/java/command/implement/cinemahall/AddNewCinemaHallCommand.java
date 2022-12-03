6e3d87c856f6afa2a8b6b2f105a367bae88133c2
alyonanov <unixcats@gmail.com>
2022-12-03 19:59:17 +0300

    add cinema hall commands

--- src/main/java/command/implement/cinemahall/AddNewCinemaHallCommand.java
+++ src/main/java/command/implement/cinemahall/AddNewCinemaHallCommand.java
@@ -1,4 +1,37 @@
 package command.implement.cinemahall;
 
-public class AddNewCinemaHallCommand {
+import usage.cooper.ClientRequest;
+import usage.cooper.ServerResponse;
+import service.CinemaHallService;
+import service.exception.ServiceException;
+import service.implement.CinemaHallServiceImplement;
+import command.Command;
+import command.exception.CommandException;
+import java.util.Map;
+
+public class AddNewCinemaHallCommand implements Command {
+
+    private CinemaHallService service;
+    private ClientRequest request;
+    private ServerResponse response;
+
+    public AddNewCinemaHallCommand(ClientRequest request, ServerResponse response) {
+        this.service = CinemaHallServiceImplement.getInstance();
+        this.request = request;
+        this.response = response;
+    }
+
+    @Override
+    public ServerResponse execute() throws CommandException {
+
+        Map<String, Object> data = request.getData();
+        String text = (String) data.get("hallType");
+        int hallSeatsNumber = (int) data.get("hallSeatsNumber");
+        try {
+            service.addNewCinemaHall(text, hallSeatsNumber);
+        } catch (ServiceException e) {
+            throw new CommandException(e);
+        }
+        return response;
+    }
 }
--- src/main/java/command/implement/cinemahall/DeleteCinemaHallCommand.java
+++ src/main/java/command/implement/cinemahall/DeleteCinemaHallCommand.java
@@ -1,4 +1,33 @@
 package command.implement.cinemahall;
 
-public class DeleteCinemaHallCommand {
+import usage.cooper.ClientRequest;
+import usage.cooper.ServerResponse;
+import service.CinemaHallService;
+import service.exception.ServiceException;
+import service.implement.CinemaHallServiceImplement;
+import command.Command;
+import command.exception.CommandException;
+import java.util.Map;
+
+public class DeleteCinemaHallCommand implements Command {
+
+    private ClientRequest request;
+    private ServerResponse response;
+    private CinemaHallService service;
+    public DeleteCinemaHallCommand(ClientRequest request, ServerResponse response) {
+        this.service = CinemaHallServiceImplement.getInstance();
+        this.request = request;
+        this.response = response;
+    }
+    @Override
+    public ServerResponse execute() throws CommandException {
+        Map<String, Object> data = request.getData();
+        int hallId = (int) data.get("hallId");
+        try {
+            service.deleteCinemaHall(hallId);
+        } catch (ServiceException e) {
+            throw new CommandException(e);
+        }
+        return response;
+    }
 }
